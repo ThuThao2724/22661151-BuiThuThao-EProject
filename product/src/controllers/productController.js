4a65342269ae849e7cf0deac3ddafbb68c4ccad3
ThuThao2724 <thuthaotv2004tv@gmail.com>
2025-11-03 13:48:40 +0700

    update

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -112,11 +112,12 @@ class ProductController {
     }
   }
   // ------------------------------------------------
+  
   async getProductById(req, res) {
     try {
-      const product = await Product(req.params.id);
+      const product = await Product.findById(req.params.id);
       if (!product) return res.status(404).json({ message: "Product not found" });
-      res.json(products);
+      res.json(product);
     } catch (error) {
       res.status(500).json({ message: "Server error" });
     }
